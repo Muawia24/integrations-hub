18515d5adfa0d4415d110f908562e0fa09eee878
Muawia24 <amuawia666@gmail.com>
2025-08-07 09:54:45 +0300

    Implemented fecht credentials functionality

--- backend/integrations/hubspot.py
+++ backend/integrations/hubspot.py
@@ -14,7 +14,7 @@ from redis_client import add_key_value_redis, get_value_redis, delete_key_redis
 CLIENT_ID = os.getenv("HUBSPOT_CLIENTID")
 SECRET_ID = os.getenv("HUBSPOT_SECRETID")
 
-encoded_client_id_secret = base64.b64decode(f'{CLIENT_ID}:{SECRET_ID}'.encode()).decode()
+encoded_client_id_secret = base64.b64encode(f'{CLIENT_ID}:{SECRET_ID}'.encode()).decode()
 REDIRECT_URI = 'http://localhost:8000/integrations/hubspot/oauth2callback'
 encoded_redirect_uri = urllib.parse.quote(REDIRECT_URI)
 authorization_url = f'https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&response_type=code&owner=user&redirect_uri={encoded_redirect_uri}'
@@ -78,8 +78,15 @@ async def oauth2callback_hubspot(request: Request):
     return HTMLResponse(content=close_window_script)
 
 async def get_hubspot_credentials(user_id, org_id):
-    # TODO
-    pass
+    credentials = await get_value_redis(f'hubspot_credentials:{org_id}:{user_id}')
+    if not credentials:
+        raise HTTPException(status_code=400, detail='No credentials found.')
+    credentials = json.loads(credentials)
+    if not credentials:
+        raise HTTPException(status_code=400, detail='No credentials found.')
+    await delete_key_redis(f'hubspot_credentials:{org_id}:{user_id}')
+
+    return credentials
 
 async def create_integration_item_metadata_object(response_json):
     # TODO
